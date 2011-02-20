05010cbd3cc189c0997f60a26bbfa7a736f96700
anumonk <Anu@anuroop-shahis-macbook-pro.local>
2011-02-20 19:34:41 +0000

    tidied up the prime factors generator by making it more succint

--- prime-factors/src/main/java/org/anumonk/katas/primefactors/PrimeFactors.java
+++ prime-factors/src/main/java/org/anumonk/katas/primefactors/PrimeFactors.java
@@ -3,23 +3,33 @@ package org.anumonk.katas.primefactors;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Attempt at the prime-factors kata, as specified by Uncle Bob. See
+ * http://butunclebob.com/ArticleS.UncleBob.ThePrimeFactorsKata for more info
+ * regarding this particular Kata.
+ * 
+ * @author anumonk
+ * 
+ */
 public class PrimeFactors {
 
+	/**
+	 * Generates a list of prime-factors for a given integer greater than 1.
+	 * 
+	 * @param n
+	 *            the number for which to generate prime-factors.
+	 * @return a list of prime-factors in numerical sequence or an empty list if
+	 *         n was less than 1.
+	 */
 	public List<Integer> generate(Integer n) {
 		ArrayList<Integer> primeFactors = new ArrayList<Integer>();
-		if (n > 1) {
-			int p = 2;
-			while (p < n) {
-				while (n % p == 0) {
-					primeFactors.add(p);
-					n /= p;
-				}
-				p ++;
-			}
-			if (n > 1) {
-				primeFactors.add(n);
+
+		for (int p = 2; n > 1; p++) {
+			for (; n % p == 0; n /= p) {
+				primeFactors.add(p);
 			}
 		}
+
 		return primeFactors;
 	}
 
